304a5c706e21966977357469096102567a5ed908
Isuru Fernando <isuruf@gmail.com>
2018-05-13 02:36:21 -0500

    Use tarball from webpage

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-mkdir build
-cd build
 cmake -G Ninja \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
-      ..
+      .
 ninja install
-ctest
+ctest --output-on-failure
