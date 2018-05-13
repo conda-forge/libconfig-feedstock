63a4b14bc289a338b7ab5c19a67eaf2e61aa9168
Isuru Fernando <isuruf@gmail.com>
2018-05-13 01:58:53 -0500

    x64->Win32

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,9 @@
 if /I "%vc%"=="14" (
-    msbuild lib\libconfig.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x64
-    msbuild lib\libconfig++.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x64
-    msbuild tests\tests.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x64
+    msbuild lib\libconfig.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=Win32
+    msbuild lib\libconfig++.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=Win32
+    msbuild tests\tests.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=Win32
 ) ELSE (
-    msbuild lib\libconfig.vcproj /t:Rebuild /p:Configuration=Release /p:Platform=x64
-    msbuild lib\libconfig++.vcproj /t:Rebuild /p:Configuration=Release /p:Platform=x64
-    msbuild tests\tests.vcproj /t:Rebuild /p:Configuration=Release /p:Platform=x64
+    msbuild lib\libconfig.vcproj /t:Rebuild /p:Configuration=Release /p:Platform=Win32
+    msbuild lib\libconfig++.vcproj /t:Rebuild /p:Configuration=Release /p:Platform=Win32
+    msbuild tests\tests.vcproj /t:Rebuild /p:Configuration=Release /p:Platform=Win32
 )
